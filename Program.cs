3b953eca9b0928e52a3e83972d625d14be59302e
linahalle <hallergren.lina@gmail.com>
2025-12-04 10:12:08 +0100

    Took away swagger and added more code comments
    Swagger was completely uneccesary bc we use thunderclient

--- Program.cs
+++ Program.cs
@@ -7,44 +7,54 @@ var builder = WebApplication.CreateBuilder(args);
 // Add DbContext (koppla EF core till MySQL)
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
+    //hämtar connection string från appsetting.json
     var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-    options.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 33)));
-});
 
-// Add Swagger (för test)
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+    //kopplar EF core till MySql & anger serverversion
+    options.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 33)));
+
+    /*Varför?
+    - AddDbContext registrerar databasen i DI-container  (Dependency Injection)
+    - UseMySql talar om att MySQL ska användas som databas
+    - Version behövs för att EF Core ska kunna generera    korrekt SQL*/
+});
 
 var app = builder.Build();
 
-// Swagger
-app.UseSwagger();
-app.UseSwaggerUI();
-
-//test endpoint
+//test endpoint, enkelt test för att se att API:et svarar
 app.MapGet("/", () => "EuroQuest API is running");
 
-// Minimal API: Register
+// Minimal API: Register (POST/api/user/register)
+//tar emot user-modell och sparar den i databasen 
 app.MapPost("/api/user/register", async (AppDbContext db, User user) =>
 {
     //kontrollera om e-post redan finns 
     if (await db.Users.AnyAsync(u => u.Email == user.Email))
         return Results.BadRequest(new { message = "Email already exists" });
 
+    //Lägg till användaren
     db.Users.Add(user);
+
+    //spara ändringarna i databasen 
     await db.SaveChangesAsync();
+
+    //returnera ett svar (200 OK)
     return Results.Ok(new { message = "User registered", id = user.Id });
 });
 
-//Minimal API: Login
+//Minimal API: Login (POST(api/user/login))
+//kontrollerar email + lösenord
 app.MapPost("/api/user/login", async (AppDbContext db, User loginData) =>
 {
+    //försöker hämta användare som matchar email + lösenord
     var user = await db.Users
         .SingleOrDefaultAsync(u => u.Email == loginData.Email && u.Password == loginData.Password);
 
+    //om ingen matchning = Unauthorized
     if (user == null)
         return Results.Unauthorized();
 
+    //Returnera användardata (ej lösenord)
     return Results.Ok(new
     {
         id = user.Id,
@@ -54,4 +64,4 @@ app.MapPost("/api/user/login", async (AppDbContext db, User loginData) =>
     });
 });
 
-app.Run();
+app.Run(); //startar API-appen
