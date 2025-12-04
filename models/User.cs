b6bde3c4b53e8783d51c29c854a87798da678f9b
linahalle <hallergren.lina@gmail.com>
2025-12-04 09:01:37 +0100

    added comments and explanations

--- models/User.cs
+++ models/User.cs
@@ -1,13 +1,14 @@
 using System.ComponentModel.DataAnnotations;
 
+//alla entiteter av typen models har detta namespace för att organisera 
 namespace Euroquest.Models;
-//här skriver vi in userklass utan konstruktor 
 
 public class User
 {
     //I ett Entity Framework (EF Core) API-projekt behövs get set för att EF ska kunna skriva och läsa från/till databasen 
     public int Id { get; set; }
 
+    //använder maxlength för att säga att de är en varchar istället för default longtext
     [MaxLength(100)]
     public string Name { get; set; } = "";
     [MaxLength(255)]
@@ -15,6 +16,6 @@ public class User
     [MaxLength(255)]
     public string Password { get; set; } = "";
     [MaxLength(10)]
-    public string Role { get; set; } = "customer";
+    public string Role { get; set; } = "customer"; //customer är default men finns admin också 
 }
 
