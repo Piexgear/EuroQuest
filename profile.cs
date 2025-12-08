7cc7be437997a2a82dd780d5bed51b06a52cc654
Jolle-Jay <j.lopezzz@hotmail.com>
2025-12-08 13:48:28 +0100

    Putting the vode we got today, merged with the code that was from before

---
+++ profile.cs
@@ -0,0 +1,32 @@
+namespace server;
+
+using MySql.Data.MySqlClient;
+
+
+static class Profile
+{
+    public record Get_Data(string Email, string? Name);
+    public static async Task<Get_Data?>
+    Get(Config config, HttpContext ctx)
+    {
+        Get_Data? result = null;
+
+        if (ctx.Session.IsAvailable)
+        {
+            if (ctx.Session.GetInt32("user_id") is int user_id)
+            {
+                string query = "SELECT email, name FROM users WHERE id = @id";
+                var parameters = new MySqlParameter[] { new("@id", user_id) };
+                using (var reader = await MySqlHelper.ExecuteReaderAsync(config.db, query, parameters))
+                {
+                    if (reader.Read())
+                    {
+                        result = new(reader.GetString(0), reader[1] as string);
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+}
