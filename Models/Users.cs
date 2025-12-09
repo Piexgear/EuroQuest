0da9deb715e9962f302717265e1f3fcd7169544b
Jolle-Jay <j.lopezzz@hotmail.com>
2025-12-09 11:10:37 +0100

    just updated all our tables and FK in program.cs so we can DB alter them

--- Models/Users.cs
+++ Models/Users.cs
@@ -8,16 +8,16 @@ class Users
     static List<User> users = new();
     static List<Hotels> hotel = new();
 
-    public record Get_Data(int Id, string name, string Email, string Password, int role);
+    public record Get_Data(int Id, string name, string Email, string Password);
     public static async Task<List<Get_Data>> Get(Config config)
     {
         List<Get_Data> result = new();
-        string query = "SELECT id, name, email, password, role FROM users";
+        string query = "SELECT id, name, email, password FROM user";
         using (var reader = await MySqlHelper.ExecuteReaderAsync(config.db, query))
         {
             while (reader.Read())
             {
-                result.Add(new(reader.GetInt32(0), reader.GetString(1), reader.GetString(2), reader.GetString(3), reader.GetInt32(4)));
+                result.Add(new(reader.GetInt32(0), reader.GetString(1), reader.GetString(2), reader.GetString(3)));
             }
         }
         return result;
@@ -28,7 +28,7 @@ class Users
     public static async Task<GetById_Data?> GetById(int id, Config config)
     {
         GetById_Data? result = null;
-        string query = "SELECT email, FROM users WHERE id = @id";
+        string query = "SELECT email, FROM user WHERE id = @id";
         var parameters = new MySqlParameter[] { new("@id", id) };
 
         using (var reader = await MySqlHelper.ExecuteReaderAsync(config.db, query, parameters))
@@ -44,10 +44,10 @@ class Users
 
 
 
-    public record Post_Args(string Name, string Email, string Password, int Role);
+    public record Post_Args(string Name, string Email, string Password);
     public static async Task Post(Post_Args user, Config config)
     {
-        string querry = "INSERT INTO user(name, email, password role) VALUES(@name, @email, @password, @role)";
+        string querry = "INSERT INTO user(name, email, password) VALUES(@name, @email, @password)";
 
         //indexerar själv för inmatning av data 
         var parameters = new MySqlParameter[]
@@ -55,7 +55,6 @@ class Users
             new("@email", user.Email),
             new("@name", user.Name),
             new("@password", user.Password),
-            new("@role", user.Role)
 
         };
 
@@ -71,5 +70,5 @@ class Users
 
 }
 
-record User(string Name, string Email, string Password, int Role);
+record User(string Name, string Email, string Password);
 
--- Program.cs
+++ Program.cs
@@ -32,6 +32,7 @@ app.MapGet("/users{id}", Users.GetById);
 
 app.MapDelete("/users{id}", Users.Delete);
 app.MapPost("/users", Users.Post);
+app.UseSession();
 
 app.MapDelete("/db", db_reset_to_default);
 
@@ -42,71 +43,156 @@ async Task db_reset_to_default(Config config)
 {
 
     string users_create = """
-CREATE TABLE users
-(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    name (VARCHAR255),
-    email VARCHAR(256) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    role ENUM('admin', 'customer') NOT NULL DEFAULT 'customer'
-)
+CREATE TABLE IF NOT EXISTS `user` (
+	`id` INTEGER NOT NULL AUTO_INCREMENT,
+	`name` VARCHAR(255),
+	`email` VARCHAR(255) NOT NULL UNIQUE,
+	`password` VARCHAR(255) NOT NULL,
+	`role` ENUM('admin', 'customer') NOT NULL DEFAULT 'customer',
+	PRIMARY KEY(`id`)
+);
 """;
 
     string countries_create = """
-    CREATE TABLE countries
-    (
-        id INTEGER NOT NULL AUTO_INCREMENT,
-	    country_name VARCHAR(255) NOT NULL
-    )
+    CREATE TABLE IF NOT EXISTS `countries` (
+	`id` INTEGER NOT NULL AUTO_INCREMENT,
+	`country_name` VARCHAR(255) NOT NULL,
+	PRIMARY KEY(`id`)
+);
 """;
 
     string cities_create = """
-    CREATE TABLE cities
-    (
-    )
-    """;
+    CREATE TABLE IF NOT EXISTS `cities` (
+	`id` INTEGER NOT NULL AUTO_INCREMENT,
+	`city_name` VARCHAR(255) NOT NULL,
+	`country` INTEGER NOT NULL,
+	PRIMARY KEY(`id`)
+);
+""";
 
     string hotels_create = """
-    CREATE TABLE hotels
-    (
-    )
-    """;
+   CREATE TABLE IF NOT EXISTS `hotels` (
+	`id` INTEGER NOT NULL AUTO_INCREMENT,
+	`name` VARCHAR(255) NOT NULL,
+	`city` INTEGER NOT NULL,
+	`amount_of_rooms` INTEGER NOT NULL,
+	`description` TEXT(65535) NOT NULL,
+	`beach_distance` INTEGER NOT NULL COMMENT 'avstånd till strand',
+	`pool` TINYINT NOT NULL,
+	`breakfast` TINYINT NOT NULL,
+	`center_distance` INTEGER NOT NULL,
+	PRIMARY KEY(`id`)
+);
+""";
 
     string rooms_create = """
-    CREATE TABLE rooms
-    (
-    )
-    """;
+  CREATE TABLE IF NOT EXISTS `rooms` (
+	`id` INTEGER NOT NULL AUTO_INCREMENT,
+	`number` INTEGER NOT NULL,
+	`hotels_id` INTEGER NOT NULL,
+	`capacity` INTEGER NOT NULL,
+	`price` INTEGER NOT NULL,
+	PRIMARY KEY(`id`)
+);
+""";
 
     string activities_create = """
-    CREATE TABLE activities
-    (
-    )
-    """;
+    CREATE TABLE IF NOT EXISTS `activities` (
+	`id` INTEGER NOT NULL AUTO_INCREMENT,
+	`name` VARCHAR(255) NOT NULL,
+	`duration` INTEGER NOT NULL,
+	`price` INTEGER NOT NULL,
+	`address` VARCHAR(255) NOT NULL,
+	`city` INTEGER NOT NULL,
+	`capacity` INTEGER NOT NULL,
+	`description` TEXT(65535) NOT NULL,
+	PRIMARY KEY(`id`)
+);
+""";
 
     string packages_create = """
-    CREATE TABLE packages
-    (
-    )
-    """;
+    CREATE TABLE IF NOT EXISTS `packages` (
+	`id` INTEGER NOT NULL AUTO_INCREMENT,
+	`hotel_id` INTEGER NOT NULL,
+	PRIMARY KEY(`id`)
+);
+""";
 
     string packages_activities_create = """
-    CREATE TABLE packages_activities
-    (
-    )
-    """;
+    CREATE TABLE IF NOT EXISTS `packages_activities` (
+	`package_id` INTEGER NOT NULL,
+	`activity_id` INTEGER NOT NULL,
+	PRIMARY KEY(`package_id`, `activity_id`)
+);
+""";
 
     string bookings_create = """
-    CREATE TABLE bookings
-    (
-    )
-    """;
+    CREATE TABLE IF NOT EXISTS `bookings` (
+	`id` INTEGER NOT NULL AUTO_INCREMENT,
+	`package_id` INTEGER NOT NULL,
+	`user_id` INTEGER NOT NULL,
+	`check_in` DATE NOT NULL,
+	`check_out` DATE NOT NULL,
+	`guests` INTEGER NOT NULL,
+	PRIMARY KEY(`id`)
+);
+""";
 
     string rooms_bookings_create = """
-    CREATE TABLE rooms_bookings
-    (
-    )
-    """;
+    CREATE TABLE IF NOT EXISTS `rooms_bookings` (
+	`booking_id` INTEGER NOT NULL,
+	`room_id` INTEGER NOT NULL,
+	PRIMARY KEY(`booking_id`, `room_id`)
+);
+""";
+
+    string foreign_keys = """
+ALTER TABLE `cities`
+ADD FOREIGN KEY(`country`) REFERENCES `countries`(`id`)
+ON UPDATE NO ACTION ON DELETE NO ACTION;
+
+    ALTER TABLE `hotels`
+ADD FOREIGN KEY(`city`) REFERENCES `cities`(`id`)
+ON UPDATE NO ACTION ON DELETE NO ACTION;
+
+    ALTER TABLE `rooms`
+ADD FOREIGN KEY(`hotels_id`) REFERENCES `hotels`(`id`)
+ON UPDATE NO ACTION ON DELETE NO ACTION;
+
+    ALTER TABLE `activities`
+ADD FOREIGN KEY(`city`) REFERENCES `cities`(`id`)
+ON UPDATE NO ACTION ON DELETE NO ACTION;
+
+    ALTER TABLE `packages`
+ADD FOREIGN KEY(`hotel_id`) REFERENCES `hotels`(`id`)
+ON UPDATE NO ACTION ON DELETE NO ACTION;
+
+    ALTER TABLE `packages_activities`
+ADD FOREIGN KEY(`package_id`) REFERENCES `packages`(`id`)
+ON UPDATE NO ACTION ON DELETE NO ACTION;
+
+    ALTER TABLE `packages_activities`
+ADD FOREIGN KEY(`activity_id`) REFERENCES `activities`(`id`)
+ON UPDATE NO ACTION ON DELETE NO ACTION;
+
+    ALTER TABLE `bookings`
+ADD FOREIGN KEY(`package_id`) REFERENCES `packages`(`id`)
+ON UPDATE NO ACTION ON DELETE NO ACTION;
+
+    ALTER TABLE `bookings`
+ADD FOREIGN KEY(`user_id`) REFERENCES `user`(`id`)
+ON UPDATE NO ACTION ON DELETE NO ACTION;
+
+    ALTER TABLE `rooms_bookings`
+ADD FOREIGN KEY(`booking_id`) REFERENCES `bookings`(`id`)
+ON UPDATE NO ACTION ON DELETE NO ACTION;
+
+    ALTER TABLE `rooms_bookings`
+ADD FOREIGN KEY(`room_id`) REFERENCES `rooms`(`id`)
+ON UPDATE NO ACTION ON DELETE NO ACTION;
+""";
+
+
 
     await MySqlHelper.ExecuteNonQueryAsync(config.db, "DROP TABLE IF EXISTS users");
     await MySqlHelper.ExecuteNonQueryAsync(config.db, users_create);
