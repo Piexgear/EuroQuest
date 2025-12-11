c06b3c976b966e98eebc590145e042d83d8c971a
jenjira185 <jenjira.h04@gmail.com>
2025-12-11 16:01:44 +0100

    Add a comment
    Replaced my codes to Bookings.cs.
    
    Fixed 2 errors with DateTime and DateOnly.

--- Models/Booking.cs
+++ Models/Booking.cs
@@ -1,29 +1,157 @@
 namespace server;
 
+using System.Security.Cryptography.X509Certificates;
 using MySql.Data.MySqlClient;
 
 class Bookings
 {
-    static List<Hotels> booking = new();
+    static List<Bookings> booking = new();
 
 
-    // finns inget kul att visa så man kan ta bort hela blocket
-    /*
-        public record Get_Data(int Id, string City, int CoutryId);
-        public static async Task<List<Get_Data>> Get(Config config)
+    // Visa vilka bokningar användara har!   ADMIN VIEW 
+    public record Get_Data(int BookingId, string Customer, string Country, string City, string Hotel, int Rooms, DateTime CheckIn, DateTime CheckOut, int guests);
+    public static async Task<List<Get_Data>> GetBookings(Config config)
+    {
+        List<Get_Data> result = new();
+        string query = """
+            SELECT 
+            b.id AS booking_id,
+            u.name AS customer_name,
+            c.country_name,
+            ci.city_name,
+            h.name AS hotel_name,
+            r.number AS room_number,
+            b.check_in,
+            b.check_out,
+            b.guests
+            FROM bookings b
+            JOIN user u 
+            ON b.user = u.id
+            JOIN package p 
+            ON b.package = p.id
+            JOIN hotels h 
+            ON p.hotel = h.id
+            JOIN city ci 
+            ON h.city = ci.id
+            JOIN country c 
+            ON ci.country = c.id
+            LEFT JOIN room_booking rb 
+            ON b.id = rb.booking
+            LEFT JOIN rooms r 
+            ON rb.room = r.id
+            ORDER BY b.id, r.number;
+        """;
+
+        using (var reader = await MySqlHelper.ExecuteReaderAsync(config.db, query))
         {
-            List<Get_Data> result = new();
-            string query = "SELECT id, city_name, country_id FROM city";
-            using(var reader = await MySqlHelper.ExecuteReaderAsync(config.db, query))
+            while (reader.Read())
             {
-                while(reader.Read())
-                {
-                    result.Add(new(reader.GetInt32(0), reader.GetString(1), reader.GetInt32(2)));
-                }
+                result.Add(new Get_Data(reader.GetInt32(0), reader.GetString(1), reader.GetString(2), reader.GetString(3), reader.GetString(4), reader.IsDBNull(5) ? 0 : reader.GetInt32(5), reader.GetDateTime(6), reader.GetDateTime(7), reader.GetInt32(8)
+                ));
             }
+        }
+        return result;
+    }
+
+
+
+
+
+
+
+
+    // =====================Mål att göra denna för den inloggade användaren=========================
+    public record GetByUser_Data(int BookingId, string Customer, string Country, string City, string Hotel, int Rooms, DateTime CheckIn, DateTime CheckOut, int guests);
+    public static async Task<List<Get_Data>> GetByUser_data(GetByUser_Data userid, Config config, HttpContext ctx)
+    {
+        List<Get_Data> result = new();
+        bool loggedIn = false;
+        string query = """
+            SELECT 
+            b.id AS booking_id,
+            u.name AS customer_name,
+            c.country_name,
+            ci.city_name,
+            h.name AS hotel_name,
+            r.number AS room_number,
+            b.check_in,
+            b.check_out,
+            b.guests
+            FROM bookings b
+            JOIN user u ON b.user = u.id
+            JOIN package p ON b.package = p.id
+            JOIN hotels h ON p.hotel = h.id
+            JOIN city ci ON h.city = ci.id
+            JOIN country c ON ci.country = c.id
+            LEFT JOIN room_booking rb ON b.id = rb.booking
+            LEFT JOIN rooms r ON rb.room = r.id
+            ORDER BY b.id, r.number;
+        """;
+        var parameters = new MySqlParameter[]
+        {
+            new("@UserId", userid.Customer)
+        };
+
+
+        object query_result = await MySqlHelper.ExecuteScalarAsync(config.db, query, parameters);
+        if (query_result is int id)
+        {
+            if (ctx.Session.IsAvailable)
+            {
+                loggedIn = true;
+            }
+        }
+
+        using (var reader = await MySqlHelper.ExecuteReaderAsync(config.db, query))
+        {
+            while (reader.Read())
+            {
+                result.Add(new Get_Data(reader.GetInt32(0), reader.GetString(1), reader.GetString(2), reader.GetString(3), reader.GetString(4), reader.IsDBNull(5) ? 0 : reader.GetInt32(5), reader.GetDateTime(6), reader.GetDateTime(7), reader.GetInt32(8)
+                ));
+            }
+        }
+        if (!loggedIn)
+        {
+            return null;
+        }
+        else
+        {
             return result;
         }
-        */
+    }
+}
 
 
+class AmountOfPeople
+{
+    public record Get_Data(
+        int Id,
+        int Package,
+        int User,
+        DateOnly CheckIn,
+        DateOnly CheckOut,
+        int Guests
+    );
+
+    public static async Task<List<Get_Data>> Get(Config config)
+    {
+        List<Get_Data> result = new();
+        string query = "SELECT id, package, user, check_in, check_out, guests FROM bookings";
+
+        using (var reader = await MySqlHelper.ExecuteReaderAsync(config.db, query))
+        {
+            while (reader.Read())
+            {
+                result.Add(new(
+                    reader.GetInt32(0),
+                    reader.GetInt32(1),
+                    reader.GetInt32(2),
+                    DateOnly.FromDateTime(reader.GetDateTime(3)),
+                    DateOnly.FromDateTime(reader.GetDateTime(4)),
+                    reader.GetInt32(5)
+                ));
+            }
+        }
+        return result;
+    }
 }
