707e0d2ad0ad9672bfd6dc6ae36fd1bc053ca145
David Puscas <davidpuscas@live.se>
2025-12-11 14:43:23 +0100

    trying to add logic
    trying to only show for the logged in user

--- Models/Booking.cs
+++ Models/Booking.cs
@@ -1,5 +1,6 @@
 namespace server;
 
+using System.Security.Cryptography.X509Certificates;
 using MySql.Data.MySqlClient;
 
 class Bookings
@@ -60,10 +61,11 @@ class Bookings
 
 
             // =====================Mål att göra denna för den inloggade användaren=========================
-        public record GetByUser_Data(int BookingId, string Customer, string Country, string City, string Hotel, int Rooms, DateTime CheckIn, DateTime CheckOut, int guests);
-    public static async Task<List<Get_Data>> GetByUser_data(Config config)
+    public record GetByUser_Data(int BookingId, string Customer, string Country, string City, string Hotel, int Rooms, DateTime CheckIn, DateTime CheckOut, int guests);
+    public static async Task<List<Get_Data>> GetByUser_data(GetByUser_Data userid ,Config config, HttpContext ctx)
     {
         List<Get_Data> result = new();
+        bool loggedIn = false;
         string query = """
             SELECT 
             b.id AS booking_id,
@@ -76,24 +78,32 @@ class Bookings
             b.check_out,
             b.guests
             FROM bookings b
-            JOIN user u 
-            ON b.user = u.id
-            JOIN package p 
-            ON b.package = p.id
-            JOIN hotels h 
-            ON p.hotel = h.id
-            JOIN city ci 
-            ON h.city = ci.id
-            JOIN country c 
-            ON ci.country = c.id
-            LEFT JOIN room_booking rb 
-            ON b.id = rb.booking
-            LEFT JOIN rooms r 
-            ON rb.room = r.id
+            JOIN user u ON b.user = u.id
+            JOIN package p ON b.package = p.id
+            JOIN hotels h ON p.hotel = h.id
+            JOIN city ci ON h.city = ci.id
+            JOIN country c ON ci.country = c.id
+            LEFT JOIN room_booking rb ON b.id = rb.booking
+            LEFT JOIN rooms r ON rb.room = r.id
+            WHERE u.id = @UserId
             ORDER BY b.id, r.number;
         """;
+        var parameters = new MySqlParameter[]
+        {
+            new("@UserId", userid.Customer)
+        };
+  
 
-       using(var reader = await MySqlHelper.ExecuteReaderAsync(config.db, query))
+        object query_result = await MySqlHelper.ExecuteScalarAsync(config.db, query, parameters);
+        if (query_result is int id)
+        {
+            if (ctx.Session.IsAvailable)
+            {
+                loggedIn = true;
+            }
+        }
+
+        using(var reader = await MySqlHelper.ExecuteReaderAsync(config.db, query))
         {
             while(reader.Read())
             {
@@ -101,9 +111,14 @@ class Bookings
                 ));
             }
         }
-        return result;
+        if (!loggedIn)
+        {
+            return null;
+        }
+        else
+        {
+        return result;  
+        }
     }
-
-
-
+    
 }
