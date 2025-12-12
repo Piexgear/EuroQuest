c01fb9e581cb1cfaa2b805c8446b7882ea41d450
Jolle-Jay <j.lopezzz@hotmail.com>
2025-12-12 11:24:32 +0100

    Made new view to see all rooms in each hotel

--- Database/views_with_activity.sql
+++ Database/views_with_activity.sql
@@ -6,3 +6,16 @@ from hotels
     join countries on countries.id = cities.country
     join activities on activities.city = cities.id
 where city_name = 'barcelona'
+
+===========================================================
+
+create view All_activities AS
+select hotels.id, hotels.name as hotel, city_name as city, country_name as country, activities.name as activity
+from hotels
+    join cities on cities.id = hotels.city
+    join countries on countries.id = cities.country
+    join activities on activities.city = cities.id
+
+    // Skapar view för att se alla activities
+
+    
